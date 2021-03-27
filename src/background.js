0b6c065f8e79e3562aae473dd9ac718ccb02e753
untari <sastraembun0306@gmail.com>
2021-03-27 15:07:00 +0700

    adding system info

--- src/background.js
+++ src/background.js
@@ -1,12 +1,4 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create(
-    "index.html",
-      innerBounds: {
-        width: 960,
-        height: 540,
-        minWidth: 340,
-        minHeight: 540
-      }
-    }
-  );
+chrome.system.cpu.getInfo(function(info){       
+   console.log(JSON.stringify(info));   
 });
+
