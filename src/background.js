1de041f1c8c9daeba10d52dbdd6a7d805269eb12
untari <sastraembun0306@gmail.com>
2021-04-01 03:41:05 +0700

    send and recieve message from chrome extension

--- src/background.js
+++ src/background.js
@@ -1,10 +1,28 @@
 console.log('background running');
 
-chrome.browserAction.onClicked.addListener(buttonClicked)
+//chrome.browserAction.onClicked.addListener(function () {
+  //  chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
+//});
 
-function buttonClicked(tab) {
-    let msg = {
-        txt: "hola!"
+
+console.log('background running');
+
+//chrome.browserAction.onClicked.addListener(function () {
+  //  chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
+//});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        chrome.tabs.query({ active: true }, function(tabs) {
+            const msg = "Hello from background 🔥";
+            chrome.tabs.sendMessage(tabs[0].id, { "message": msg });
+        })
     }
-    chrome.tabs.sendMessage(tab.id, msg)
-}
+});
+
+// Listening to messages page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    // Callback for that request
+    sendResponse({ message: "Background has received that message 🔥" });
+});
