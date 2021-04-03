f5e08a881a0fafee397f4ad7187d7b8073777ce2
untari <sastraembun0306@gmail.com>
2021-04-04 01:58:43 +0700

    working version message passing cpu value

--- src/background.js
+++ src/background.js
@@ -2,11 +2,28 @@ console.log('background running');
 //chrome.browserAction.onClicked.addListener(function () {
   //  chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
 //});
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { 
-    var cpuInfo = chrome.system.cpu.getInfo(function(cpuInfo) {
-            console.log(cpuInfo);
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        chrome.tabs.query({ active: true }, function(tabs) {
+        // const msg = "Hello from background 🔥";
+        chrome.system.cpu.getInfo(function(cpuInfo) {
+                    console.log(JSON.stringify(cpuInfo));
+            chrome.tabs.sendMessage(tabs[0].id, JSON.stringify(cpuInfo), function(response) {
+                    console.log(response);
+            });
         });
-  chrome.tabs.sendMessage(tabs[0].id, cpuInfo, function(response) {
-    console.log(response);
-  });
+            //chrome.tabs.sendMessage(tabs[0].id, { "message": msg }, function(response){
+                // console.log(response);
+                //});
+        });
+    }
+});
+
+// Listening to messages page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    // Callback for that request
+    sendResponse({ message: "Background has received that message 🔥" });
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,10 +1,16 @@
 console.log('content script');
 
+const msg = 'Hello from content Script ⚡'
+chrome.runtime.sendMessage({ message: msg }, function(response) {
+    console.log(response);
+});
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    return true;
-  });
+// Listening to messages in Context Script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    // Callback
+    console.log('hello world');
+    sendResponse({ message: 'Content script has received that message ⚡' })
+    
+})
+
