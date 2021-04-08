6773aabc11e93ce9dae5184b4180d1ebbb753119
untari <sastraembun0306@gmail.com>
2021-04-09 00:11:13 +0700

    send message  memory info

--- src/background.js
+++ src/background.js
@@ -7,17 +7,25 @@ console.log('background running');
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
      (function setInterval(){
+         //big json
             chrome.tabs.query({ active: true }, function(tabs) {
-            // const msg = "Hello from background 🔥";
-            chrome.system.cpu.getInfo(function(cpuInfo) {
-                        console.log(JSON.stringify(cpuInfo));
-                chrome.tabs.sendMessage(tabs[0].id, JSON.stringify(cpuInfo), function(response) {
-                        console.log(response);
+                // const msg = "Hello from background 🔥";
+                chrome.system.cpu.getInfo(function(cpuInfo) {
+                            console.log(JSON.stringify(cpuInfo));
+                chrome.tabs.sendMessage(tabs[0].id,  JSON.stringify({type: 'cpu', data: cpuInfo}),  function(response) {
+                            console.log(response);
+                    });
                 });
             });
-                //chrome.tabs.sendMessage(tabs[0].id, { "message": msg }, function(response){
-                    // console.log(response);
-                    //});
+            //memoryUsage
+             chrome.tabs.query({ active: true }, function(tabs) {
+                // const msg = "Hello from background 🔥";
+                chrome.system.memory.getInfo(function(memoryUsage) {
+                            console.log(JSON.stringify(memoryUsage));
+                chrome.tabs.sendMessage(tabs[0].id,  JSON.stringify({type: 'memory', data: memoryUsage}),  function(response) {
+                            console.log(response);
+                    });
+                });
             });
             setTimeout(setInterval, 5000);
         })();
